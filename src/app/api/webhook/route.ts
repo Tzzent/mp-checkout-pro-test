0701870ef1f0678fe35d6c1389b1f0f9f8e2a44e
Tzzent <cr9474320@gmail.com>
2024-12-31 23:37:36 -0500

    fix: webhook to handle notifications

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,24 +1,32 @@
 import { Payment } from "mercadopago"
 import { revalidatePath } from "next/cache"
+import { type NextRequest, NextResponse } from "next/server"
 
 import { mercadopago } from "@/api"
 
-type RequestBody = {
-  data: {
-    id: string
+export const POST = async (req: NextRequest) => {
+  try {
+    const { searchParams } = req.nextUrl
+
+    const id = searchParams.get("id")
+    const topic = searchParams.get("topic")
+
+    if (!id || topic !== "payment") {
+      console.error("Missing or invalid parameters")
+      throw new Error("Missing or invalid parameters")
+    }
+
+    const payment = await new Payment(mercadopago).get({
+      id: Number(id)
+    })
+
+    if (payment.status === "approved") {
+      revalidatePath("/")
+    }
+
+    return new NextResponse("OK", { status: 200 })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 })
   }
-}
-
-export const POST = async (req: Request) => {
-  const body: RequestBody = await req.json()
-
-  const payment = await new Payment(mercadopago).get({
-    id: body.data.id,
-  })
-
-  if (payment.status === "approved") {
-    revalidatePath("/")
-  }
-
-  return new Response("OK", { status: 200 })
 }
